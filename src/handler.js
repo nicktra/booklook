239ea7d723f47de39cbeeb2e96b424b420584549
baloh <balohbaloh@yahoo.co.id>
2021-04-24 14:19:50 +0700

    Add getAllBooksHandler, getBookByIdHandler

--- src/handler.js
+++ src/handler.js
@@ -64,6 +64,50 @@ const addBookHandler = (request, h) => {
   return response;
 };
 
+const getAllBooksHandler = (request, h) => {
+  const { name, reading, finished } = request.query;
+  let filterBook = books;
+
+  // eslint-disable-next-line max-len
+  if (name) filterBook = filterBook.filter((book) => book.name.toLowerCase().includes(name.toLowerCase()));
+  if (reading) filterBook = filterBook.filter((book) => book.reading === (reading === '1' ? true : false));
+  if (finished) filterBook = filterBook.filter((book) => book.finished === (finished === '1' ? true : false));
+
+  const response = h.response({
+    status: 'success',
+    data: {
+      books: filterBook.map((book) => ({
+        id: book.id,
+        name: book.name,
+        publisher: book.publisher,
+      })),
+    },
+  }).code(200);
+  return response;
+};
+
+const getBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const book = books.filter((n) => n.id === bookId)[0];
+
+  if (book !== undefined) {
+    const response = h.response({
+      status: 'success',
+      data: {
+        book,
+      },
+    }).code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  }).code(404);
+  return response;
+};
+
 module.exports = {
-  addBookHandler,
+  addBookHandler, getAllBooksHandler, getBookByIdHandler,
 };
